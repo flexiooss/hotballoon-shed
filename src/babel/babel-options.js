9af6ffc7d8206acd36d4d9b97fc6c0870e425cea
thomas <thomas@flexio.fr>
2018-11-13 09:46:54 +0100

    try to fix modules resolver issue

--- src/babel/babel-options.js
+++ src/babel/babel-options.js
@@ -5,7 +5,8 @@ const path = require('path')
 module.exports = {
   presets: [
     [
-      path.resolve(__dirname, '../../node_modules/@babel/preset-env'),
+      // path.resolve(__dirname, '../../node_modules/@babel/preset-env'),
+      'preset-env',
       {
         targets: {
           'browsers': [
