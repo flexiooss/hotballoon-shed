234ec0c234f24b16b744d5bb975cdb8b9ff12d48
tomacht <thomas@flexio.fr>
2019-05-07 10:42:24 +0200

    test python3 v

--- hotballoon-shed.sh
+++ hotballoon-shed.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 source ${SCRIPT_DIR}/venv/bin/activate
-python3.7 ${SCRIPT_DIR}/src/main.py "$@"
+python3 ${SCRIPT_DIR}/src/main.py "$@"
 deactivate
--- venv.sh
+++ venv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
-if [ ! -f /usr/bin/python3.7 ]; then
-    echo "Python 3.7 is required"
+if [ ! -f /usr/bin/python3 ]; then
+    echo "Python 3 is required"
     exit 1
 fi
 
@@ -10,11 +10,11 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 cd ${SCRIPT_DIR}
 
-python3.7 -m venv ${SCRIPT_DIR}/venv
+python3 -m venv ${SCRIPT_DIR}/venv
 source ${SCRIPT_DIR}/venv/bin/activate
-python3.7 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 set +e
-python3.7 -m pip install -r ${SCRIPT_DIR}/requirements.txt
+python3 -m pip install -r ${SCRIPT_DIR}/requirements.txt
 
 cd ${CURRENT_PWD}
