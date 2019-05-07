fc53c282627c3fc40a07eb27422abb74e108a744
tomacht <thomas@flexio.fr>
2019-05-07 14:34:56 +0200

    fix venv python version for ci

--- venv.sh
+++ venv.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 set -e
-if [ ! -f /usr/bin/python3 ]; then
-    echo "Python 3 is required"
-    exit 1
-fi
 
 CURRENT_PWD=$PWD
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 cd ${SCRIPT_DIR}
 
-python3 -m venv ${SCRIPT_DIR}/venv
+python3.7 -m venv ${SCRIPT_DIR}/venv
 source ${SCRIPT_DIR}/venv/bin/activate
-python3 -m pip install --upgrade pip
+python3.7 -m pip install --upgrade pip
 
 set +e
-python3 -m pip install -r ${SCRIPT_DIR}/requirements.txt
+python3.7 -m pip install -r ${SCRIPT_DIR}/requirements.txt
 
 cd ${CURRENT_PWD}
