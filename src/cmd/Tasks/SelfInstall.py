26b7098748189c5074cb949f0eb9ed96903f496c
tomacht <thomas@flexio.fr>
2019-05-13 18:28:32 +0200

    fix alpine extract unzip fail

--- src/cmd/Tasks/SelfInstall.py
+++ src/cmd/Tasks/SelfInstall.py
@@ -1,7 +1,9 @@
 import os
+import re
 import shutil
 from pathlib import Path
 from subprocess import Popen
+from typing import Optional
 
 from cmd.Directories import Directories
 from cmd.Tasks.Task import Task
@@ -39,8 +41,25 @@ class SelfInstall(Task):
             ['mvn', '-Dartifact=org.codingmatters.value.objects:cdm-value-objects-js:' + version + ':zip:embedded',
              'dependency:copy', '-DoutputDirectory=' + lib.as_posix()])
 
-        child: Popen = Popen(['unzip', 'cdm-value-objects-js-*-embedded.zip', '-d', Directories.VALUE_OBJECT_GENERATOR],
+        generator: Path = Path(lib / Directories.VALUE_OBJECT_GENERATOR)
+        generator.mkdir()
+        print('****     create ' + generator.as_posix())
+
+        archive: Optional[Path] = None
+
+        for file in os.listdir(lib.as_posix()):
+            if re.match(re.compile('^cdm-value-objects-js-.*-embedded\.zip$'), file) is not None:
+                archive = Path(lib / file)
+
+        if archive is None or not archive.is_file():
+            raise FileNotFoundError('No `cdm-value-objects-js-*-embedded.zip` archive found')
+
+        print('****     archive found : ' + archive.as_posix())
+
+        child: Popen = Popen(['unzip', archive.as_posix(), '-d', Directories.VALUE_OBJECT_GENERATOR],
                              cwd=lib.as_posix())
+        print('****     unzip ' + archive.as_posix() + ' into ' + Directories.VALUE_OBJECT_GENERATOR)
+
         child.communicate()
 
     def process(self):
