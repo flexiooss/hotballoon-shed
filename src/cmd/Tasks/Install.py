ece11c50f2a607f2b880ca087c423251fbb972f6
tomacht <thomas@flexio.fr>
2019-05-31 08:33:17 +0200

    module import to root package

--- src/cmd/Tasks/Install.py
+++ src/cmd/Tasks/Install.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+from typing import Optional
+
+from cmd.Options import Options
 from cmd.Tasks.Task import Task
 from cmd.Tasks.Tasks import Tasks
+from cmd.package.PackageHandler import PackageHandler
 from cmd.package.modules.Module import Module
 from cmd.package.modules.ModulesHandler import ModulesHandler
 
@@ -7,16 +12,25 @@ from cmd.package.modules.ModulesHandler import ModulesHandler
 class Install(Task):
     NAME = Tasks.INSTALL
 
+    def __init__(self, options: Options, package: Optional[PackageHandler], cwd: Path,
+                 node_modules: Optional[Path] = None) -> None:
+        super().__init__(options, package, cwd)
+        self.__node_modules: Optional[Path] = node_modules
+        self.__ensure_node_modules()
+
+    def __ensure_node_modules(self):
+        if self.__node_modules is None:
+            self.__node_modules = self.cwd
+
     def __modules_install(self):
         if self.package.config().has_modules():
             modules: ModulesHandler = ModulesHandler(self.package)
             module: Module
             for module in modules.modules:
-                Install(self.options, module.package, module.package.cwd).process()
+                Install(self.options, module.package, module.package.cwd, self.__node_modules).process()
 
     def process(self):
         print('INSTALL : ' + self.package.name())
-        self.exec(['npm', 'install', '--no-package-lock', '--force'])
+        self.exec(['npm', 'install', self.__node_modules.as_posix(), '--no-package-lock', '--force'])
 
         self.__modules_install()
-
