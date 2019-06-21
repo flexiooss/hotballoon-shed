69209bdd43e294e76b17c7b0b820bb511c25cf9b
tomacht <thomas@flexio.fr>
2019-06-21 14:59:32 +0200

    add last npm logs to stdout

--- src/cmd/Tasks/Publish.py
+++ src/cmd/Tasks/Publish.py
@@ -1,4 +1,7 @@
+import glob
+import os
 import sys
+from pathlib import Path
 from subprocess import Popen, PIPE
 
 from cmd.Tasks.Task import Task
@@ -7,6 +10,30 @@ from cmd.Tasks.Tasks import Tasks
 
 class Publish(Task):
     NAME = Tasks.PUBLISH
+
+    def get_last_file(self, file_pattern: str) -> str:
+        list_of_files = glob.glob(file_pattern)
+        latest_file = max(list_of_files, key=os.path.getctime)
+        return latest_file
+
+    def print_last_lines(self, filename: str, no_of_lines: int = 1):
+        file = open(filename, 'r')
+        lines = file.readlines()
+        last_lines = lines[-no_of_lines:]
+        for line in last_lines:
+            print(line)
+        file.close()
+
+    def print_last_npm_logs(self, lines: int = 50):
+        npm_log_dir: Path = Path(Path().home() / '.npm/_logs')
+        if npm_log_dir.is_dir():
+            logs: Path = Path(self.get_last_file(npm_log_dir.as_posix() + '/*.log'))
+            if logs.is_file():
+                self.print_last_lines(logs.as_posix(), lines)
+            else:
+                print('No npm file log found')
+        else:
+            print('No npm dif log found at ' + npm_log_dir.as_posix())
 
     def process(self):
         print('PUBLISH : ' + self.package.name())
@@ -21,10 +48,6 @@ class Publish(Task):
 
         print("deploying JS package in " + self.cwd.as_posix())
 
-        # print(
-        #     ['npm-cli-login', '-u', self.options.username, '-p', self.options.password, '-e', self.options.email, '-r',
-        #      self.options.registry])
-
         print('****     ****    LOGIN')
         p1 = Popen(
             ['npm-cli-login', '-u', self.options.username, '-p', self.options.password, '-e', self.options.email, '-r',
@@ -37,6 +60,7 @@ class Publish(Task):
         code = p1.returncode
         if code != 0:
             sys.stderr.write("LOGIN ****      Can't upload JS package: " + self.cwd.as_posix() + "\n")
+            self.print_last_npm_logs(50)
             sys.stderr.write("Command terminated with wrong status code: " + str(code) + "\n")
             sys.exit(code)
         print('****     ****    LOGGED')
@@ -55,6 +79,7 @@ class Publish(Task):
 
         if code != 0:
             sys.stderr.write("UNPUBLISH ****      Can't upload JS package: " + self.cwd.as_posix() + "\n")
+            self.print_last_npm_logs(50)
             sys.stderr.write("Command terminated with wrong status code: " + str(code) + "\n")
             sys.exit(code)
 
@@ -76,6 +101,8 @@ class Publish(Task):
         code = p3.returncode
 
         if code != 0:
+            sys.stderr.write("PUBLISH ****      Can't upload JS package: " + self.cwd.as_posix() + "\n")
+            self.print_last_npm_logs(50)
             sys.stderr.write("PUBLISH ****      Can't upload JS package: " + self.cwd.as_posix() + "\n")
             sys.stderr.write("Command terminated with wrong status code: " + str(code) + "\n")
             sys.exit(code)
