2ab25d8f8d6a22f1ffee6e4dca4e8b9483a11712
tomacht <thomas@flexio.fr>
2020-05-07 13:48:35 +0200

    rm strict python version

--- venv.sh
+++ venv.sh
@@ -6,11 +6,11 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 cd ${SCRIPT_DIR}
 
-python3.7 -m venv ${SCRIPT_DIR}/venv
+python3 -m venv ${SCRIPT_DIR}/venv
 source ${SCRIPT_DIR}/venv/bin/activate
-python3.7 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
 set +e
-python3.7 -m pip install -r ${SCRIPT_DIR}/requirements.txt
+python3 -m pip install -r ${SCRIPT_DIR}/requirements.txt
 
 cd ${CURRENT_PWD}
