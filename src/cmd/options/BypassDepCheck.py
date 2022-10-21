eb777785ccd169c5727a1ec66d8df2e63ca28ca9
tomacht <thomas@flexio.fr>
2022-10-21 15:58:52 +0200

    tests versions

--- src/cmd/options/BypassDepCheck.py
+++ src/cmd/options/BypassDepCheck.py
@@ -5,7 +5,7 @@ from cmd.options.Option import Option
 class BypassDepCheck(Option):
     HAS_VALUE = False
     SHORT_NAME = 'X'
-    NAME = 'bypass_dep_check'
+    NAME = 'bypass-dep-check'
 
     def exec(self) -> Options:
         self.options.bypass_dep_check = True
