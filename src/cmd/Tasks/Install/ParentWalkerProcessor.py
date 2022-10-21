bedec3de1fac53fe45c3387726bdcf24a0b70278
tomacht <thomas@flexio.fr>
2022-10-21 11:15:50 +0200

    except optionnal deps

--- src/cmd/Tasks/Install/ParentWalkerProcessor.py
+++ src/cmd/Tasks/Install/ParentWalkerProcessor.py
@@ -15,6 +15,9 @@ class ParentWalkerProcessor(WalkerProcessor):
     def count(self) -> int:
         return len(self.parents)
 
+    def __cleanVersion(self, version: str) -> bool:
+        return re.sub('[~^<=>*]', '', version)
+
     def process(self, package: PackageHandler):
         hb_package: Optional[HBShedPackageHandler] = HBShedPackageHandler.from_package_handler(package)
 
@@ -24,9 +27,9 @@ class ParentWalkerProcessor(WalkerProcessor):
 
                 if self.parents.get(hb_package.config().parent_name()):
 
-                    if hb_package.config().has_parent_version() and self.parents.get(
-                            hb_package.config().parent_name()) is not None and self.parents.get(
-                            hb_package.config().parent_name()) != hb_package.config().parent_version():
+                    if hb_package.config().has_parent_version() \
+                            and self.parents.get( hb_package.config().parent_name()) is not None \
+                            and self.__cleanVersion(self.parents.get(hb_package.config().parent_name())) != self.__cleanVersion(hb_package.config().parent_version()):
                         raise ImportError(
                             'Version conflict with parent package : ' + package.name() + ' parent: ' + hb_package.config().parent_name() + ' version:' + hb_package.config().parent_version() + ' because version : ' + self.parents.get(
                                 hb_package.config().parent_name()) + ' already registered')
