990ce27e21d3702e6add013524fe4127e501e5ce
tomacht <thomas@flexio.fr>
2024-04-08 11:18:49 +0200

    test ws

--- src/cmd/Tasks/Dev/stack_server_config.py
+++ src/cmd/Tasks/Dev/stack_server_config.py
@@ -2,12 +2,12 @@ stack_server_config: dict = {
     "host": "172.17.0.1",
     # "disableHostCheck": True,
     "static": {"publicPath": "/"},
-    "public": "https://dev.flexio.io/devui",
+    # "public": "https://dev.flexio.io/devui",
     "client": {
         "logging": 'info',
         "overlay": False,
         "webSocketTransport": 'sockjs',
-        "webSocketURL": "https://dev.flexio.io/devui/socketjs"
+        "webSocketURL": "auto://dev.flexio.io/devui/socketjs"
     },
     "proxy": [
         {
