02d6ae58747ef75792d5ec7e8f2a38bb8475ce90
tomacht <thomas@flexio.fr>
2025-10-30 10:38:14 +0100

    deported stack

---
+++ src/cmd/Tasks/Dev/stack_server_config_v2.py
@@ -0,0 +1,29 @@
+stack_server_config: dict = {
+    "host": "172.17.0.1",
+    "hot": False,
+    "liveReload": True,
+    "client": {
+        "logging": 'verbose',
+        "overlay": {
+            "errors": True,
+            "warnings": False,
+            "runtimeErrors": True,
+        },
+        "webSocketTransport": 'sockjs',
+        "webSocketURL": "auto://dev-full-v2.flexio.io/devui/socketjs"
+    },
+    "proxy": [
+        {
+            "context": [
+                "//[a-z]+/*"
+            ],
+            "logLevel": "debug",
+            "target": "https://dev-full-v2.flexio.io/devui",
+            "secure": False,
+            "pathRewrite": {
+                "^//[a-z]+/*": "/"
+            }
+        }
+    ],
+    "webSocketServer": False
+}
